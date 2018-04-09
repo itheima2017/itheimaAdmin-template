e7f7092200a01f06be2781eb1945c99bf3bfc1fb
taoshiwei <taoshiwei@itcast.cn>
2018-04-09 18:01:06 +0800

    添加 => vueSPA

---
+++ template/vueSPA/src/lang/en.js
@@ -0,0 +1,49 @@
+export default {
+  route: {
+    dashboard: 'Dashboard',
+    manage: 'manage',
+    users: 'users',
+    menus: 'menus',
+    permissions: 'permissions',
+    logs: 'logs'
+  },
+  navbar: {
+    logOut: 'Log Out',
+    dashboard: 'Dashboard',
+    github: 'Github',
+    screenfull: 'screenfull',
+    theme: 'theme'
+  },
+  login: {
+    title: 'itheima login',
+    logIn: 'Log in',
+    username: 'Username',
+    password: 'Password',
+    any: 'any',
+    thirdparty: 'Third',
+    thirdpartyTips: 'Can not be simulated on local, so please combine you own business simulation! ! !'
+  },
+  tagsView: {
+    close: 'Close',
+    closeOthers: 'Close Others',
+    closeAll: 'Close All'
+  },
+  table: {
+    title: 'Title',
+    search: 'Search',
+    addUser: 'add',
+    id: 'ID',
+    email: 'Email',
+    phone: 'Phone',
+    username: 'User',
+    permissionUser: 'Permission',
+    avatar: 'Avatar',
+    introduction: 'Introduction',
+    paddword: 'paddWord',
+    actions: 'Actions',
+    edit: 'Edit',
+    delete: 'Delete',
+    cancel: 'Cancel',
+    confirm: 'Confirm'
+  }
+}
---
+++ template/vueSPA/src/lang/zh.js
@@ -0,0 +1,49 @@
+export default {
+  route: {
+    dashboard: '首页',
+    manage: '后台管理',
+    users: '用户管理',
+    menus: '菜单管理',
+    permissions: '权限管理',
+    logs: '日志管理'
+  },
+  navbar: {
+    logOut: '退出登录',
+    dashboard: '首页',
+    github: '项目地址',
+    screenfull: '全屏',
+    theme: '换肤'
+  },
+  login: {
+    title: '黑马Admin',
+    logIn: '登录',
+    username: '账号',
+    password: '密码',
+    any: '随便填',
+    thirdparty: '第三方登录',
+    thirdpartyTips: '本地不能模拟，请结合自己业务进行模拟！！！'
+  },
+  tagsView: {
+    close: '关闭',
+    closeOthers: '关闭其它',
+    closeAll: '关闭所有'
+  },
+  table: {
+    title: '请输入用户',
+    search: '搜索',
+    addUser: '新增用户',
+    id: '序号',
+    email: '邮箱',
+    phone: '联系电话',
+    username: '用户名',
+    permissionUser: '权限组名称',
+    avatar: '头像',
+    introduction: '介绍',
+    paddword: '密码',
+    actions: '操作',
+    edit: '编辑',
+    delete: '删除',
+    cancel: '取 消',
+    confirm: '确 定'
+  }
+}
