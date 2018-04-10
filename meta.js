731a0ca0974dd154399e82e6f6156fe83466ed6f
taoshiwei <taoshiwei@itcast.cn>
2018-04-10 15:25:35 +0800

    添加 => 修复 => 更新 => print cwd

--- meta.js
+++ meta.js
@@ -140,7 +140,7 @@ module.exports = {
 
     let cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName)
     cwd = cwd + '/vueSPA'
-    console.log(cwd)
+    printMessage(cwd, green)
 
     if (data.npminstall) {
       installDependencies(cwd, data.npminstall, green)
